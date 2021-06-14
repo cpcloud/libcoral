02a23a9037c3e222c9a21a8d850a56e805b5d015
Phillip Cloud <cpcloud@gmail.com>
2021-06-14 10:08:35 -0400

    Use upstream packages

--- nix/default.nix
+++ nix/default.nix
@@ -3,10 +3,6 @@ let
 in
 import sources.nixpkgs {
   overlays = [
-    (import ../libedgetpu/nix/abseil-cpp.nix)
     (import ./eigen.nix)
-    (self: super: {
-      libedgetpu = import ../libedgetpu;
-    })
   ];
 }
